2f095f1d903ed490645c54782fdf714d6002d52d
leekery <pashokmoreplay@gmail.com>
2024-06-07 16:05:52 +0300

    current date test

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import json
 import os
 import sys
 from openai import OpenAI
+from datetime import datetime
+
+current_date = datetime.now().date()
+print(current_date)
 
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio") # local server lm-studio app
 
@@ -9,7 +13,7 @@ def process_ai_output(message):
     completion = client.chat.completions.create(
         model="tinybiggames/Phi-3-mini-4k-instruct-Q4_K_M-GGUF",
         messages=[
-            {"role": "system", "content": "Your purpose is to get from user message time for registration and place and output place and time of order in json format."},
+            {"role": "system", "content": "Your purpose is to get from user message time for registration and place and output place and time of order in json format with keys (name,time, place). list of places to set in json: `игровой клуб`, `бильярд`, `покер`, `казино`. current date ``"},
             {"role": "user", "content": message}
         ],
         temperature=0.7,
@@ -17,4 +21,4 @@ def process_ai_output(message):
 
     return(completion.choices[0].message.content)
 
-print(process_ai_output("хочу сделать резервацию на 23 мая в 13:00"))
+print(process_ai_output("Я хотел бы записаться на прием к терапевту на 15 июня в 14:30."))
